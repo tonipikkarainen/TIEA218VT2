4c4327cc730ce929b4ebf525eb7718f8ccb5f00a
Toni Pikkarainen <toni.t.pikkarainen@gmail.com>
2016-09-28 12:27:35 +0300

    valmis logiikka

--- vt2.js
+++ vt2.js
@@ -1,20 +1,18 @@
-/*javascript koodi, vt2*/
+/*javascript koodi, vt2, 
+Toni Pikkarainen
+*/
+
 "use strict";  
+
 /*
-Pitäisi saada toimimaan, että hypynjälkeen voi vain hyppiä ja 
-maalialueelta ei saa poistua
+Tästä ohjelma lähtee käyntiin, html-dokumentin antama puu on ladattu.
 */
 window.onload = function() {
 
-	var ruutuLkm = 8;
-	
+	document.getElementsByTagName("h1")[0].textContent = "Halma";
 	asetaVuoro();
 
-	//var vuoro = Math.round(Math.random()); // arpoo vuoron
-	//console.log(vuoro);
-	
 	var syottoAlue = document.getElementById("ruudukko");
-	
 
 	var field = syottoAlue.getElementsByTagName("fieldset")[0];
 	
@@ -28,33 +26,29 @@ window.onload = function() {
 	uusiField.appendChild(napOts);
 	
 	var luku = 3;
-	for(var i=0 ; i< 5 ; i++){
+	for(var i=0 ; i< 5 ; i++){//Tässä luodaan radionappulat
 		
 		var p = document.createElement("p");
 		var radioButton = document.createElement("input");
 		radioButton.setAttribute("type","radio");
 		radioButton.setAttribute("name","napMaara");
 		radioButton.setAttribute("value",i);
-		if(i == 0){
+		if(i == 4){
 			radioButton.checked=true;
 		}
 		p.appendChild(radioButton);
 		
+		
 		var arvo = document.createTextNode(luku);
 		p.appendChild(arvo);
-		
-		uusiField.appendChild(p);
 		luku = luku + (3+i);
+	
+		uusiField.appendChild(p);	
 	}
-	//
-	
-	
-	
+
 	var input = syottoAlue.getElementsByTagName("input")[0];
 	input.value="";
-	
-	
-	
+
 	var luo = document.getElementById("luo");
 	
 	var varod = document.createElement("div");
@@ -64,29 +58,17 @@ window.onload = function() {
 	field.getElementsByTagName("p")[0].appendChild(varod);
 	
 	luo.addEventListener("click", luonti);
-	
 
-	
-	luoRuudut(8,0,vuoro);
+	luoRuudut(16,5,vuoro); //Luodaan ruudut ladatessa
 
-	
 }
-/*
-function tarkistus(e){
-	
-	if (isNaN(parseInt(e.target.value)))
-	    e.target.setAttribute("class","red");
-}
-*/
 
-/*Luo uuden taulukon
-	TODO: lisää pallot soluihin!!
+/*Luo uuden ruudukon
 */
 function luonti(e){
 	e.preventDefault();
-	
-	
-	var vuoro = Math.round(Math.random());
+
+	var vuoro = Math.round(Math.random()); //Asetetaan vuoro satunnaisesti
 	
 	var syottoAlue = document.getElementById("ruudukko");
 	
@@ -108,7 +90,7 @@ function luonti(e){
 		vuoroAlue.appendChild(red);
 	}
 	else
-		vuoroAlue.appendChild(blue); ///
+	vuoroAlue.appendChild(blue); ///
 	vuoroAlue.firstChild.setAttribute("id","vuoro");
 	var input = syottoAlue.getElementsByTagName("input")[0];
 	
@@ -122,43 +104,30 @@ function luonti(e){
 		if(radioList[i].checked)
 			napLkm = parseInt(radioList[i].value);
 	}
-		
-	//console.log(napLkm);
+
 	
-	
-	
-	var syote=parseInt(input.value);
+	var syote=parseInt(input.value); //Otetaan ruudukon koko talteen
 	console.log(syote);
 
-	/*
-	if(input.value == "")
-		syote=8;
-	*/
-	console.log(syote);
-	//var varoitus = document.createTextNode("");
-	
 	var field = syottoAlue.getElementsByTagName("fieldset")[0];
-	luoRuudut(syote,napLkm,vuoro);
-    
+	luoRuudut(syote,napLkm,vuoro);  
 }
 
-		
-
-
 /*
-Täytyy varmaan muuttaa skaalautuvaksi jossainvaiheessa
+Asettaa ruudunleveydeksi 50px, tämän voisi muuttaa skaalautuvaksi.
 */
 function ruudunLeveys(){
+
 	var taulukot=document.getElementsByTagName("table");
 	var taulukko=taulukot[0];
-	var ruudunLeveys = 50;//document.clientWidth; 
-	console.log(ruudunLeveys);
+	var ruutuLeveys = 50;//document.clientWidth; 
+	console.log(ruutuLeveys);
 	var ruudut = taulukko.getElementsByTagName("td");
 	
-	/*Asetetaan leveys ja korkeus ruuduille*/
+
 	for(var i=0;i<ruudut.length ; i++){
-		ruudut[i].style.width= ruudunLeveys+"px";
-		ruudut[i].style.height= ruudunLeveys+"px";
+		ruudut[i].style.width= ruutuLeveys+"px";
+		ruudut[i].style.height= ruutuLeveys+"px";
 	}
 }
 
@@ -184,6 +153,10 @@ function ruutuVarit(){
 	}
 }
 
+/*
+Luo ruudukon ja siihen pelinappulat
+valintojen mukaisesti
+*/
 function luoRuudut(syote, napLkm, vuoro){
 	var varod = document.getElementById("varod");
 	if (isNaN(syote) || syote < 8 || syote > 16){
@@ -193,15 +166,11 @@ function luoRuudut(syote, napLkm, vuoro){
 		varod.textContent = "";		
 		var body = document.getElementsByTagName("body")[0];
 	    var taul = body.getElementsByTagName("table")[0];
-		
-		
+	
 	    body.removeChild(taul);	
 		var uusiTaul = document.createElement("table");
 		body.appendChild(uusiTaul);
 		
-		
-		
-		//var rivi = document.createElement("tr");
 		for(var i=0 ; i<syote ; i++){
 			var rivi=document.createElement("tr");
 			uusiTaul.appendChild(rivi);
@@ -215,6 +184,7 @@ function luoRuudut(syote, napLkm, vuoro){
 		           red.src = "red.svg";
 		           red.alt ="red";
 				   solu.appendChild(red);
+				   //solu.appendChild(luoPallo(syote,"red"));
 				   solu.setAttribute("title","blueMaali");
 				   }
 				   	
@@ -223,6 +193,7 @@ function luoRuudut(syote, napLkm, vuoro){
 					blue.src = "blue.svg";
 					blue.alt ="blue";
 					solu.appendChild(blue);
+					//solu.appendChild(luoPallo(syote,"blue"));
 					solu.setAttribute("title","redMaali");
 				}
 					
@@ -233,6 +204,9 @@ function luoRuudut(syote, napLkm, vuoro){
 }
 }
 
+/*
+Tähän funktioon tullaan kun on klikattu ruutua
+*/
 function klikkaus(){
 	
 	var vuoroKuva = document.getElementById("vuoro");
@@ -247,6 +221,9 @@ function klikkaus(){
 	
 }
 
+/*
+Asettaa vuoron, kun dokumentti ladataan
+*/
 function asetaVuoro(){
 	
 	var vuoro = Math.round(Math.random()); // arpoo vuoron
@@ -271,68 +248,92 @@ function asetaVuoro(){
 	//blueVuoro.setAttribute = ("id","vuoro");
 	
 	if(vuoro == 0){
-		vuoroAlue.appendChild(redVuoro);
+		vuoroAlue.appendChild(redVuoro);//(luoPallo(8,"red"));
 	}
 	else
-		vuoroAlue.appendChild(blueVuoro);
+		vuoroAlue.appendChild(blueVuoro);//(luoPallo(8,"blue"));
 	vuoroAlue.firstChild.setAttribute("id","vuoro");
 }
 
+/*
+Funktio kertoo miten toimitaan punaisen vuorolla
+*/
 function punaisenVuoro(valittu){
 	var kuvat = document.getElementsByTagName("img");
 	
 	var iIndex = valittu.parentNode.rowIndex;
 	var jIndex = valittu.cellIndex;
 	
-	//console.log("i:"+iIndex);
-	//console.log("j:"+jIndex);
-	
-	var red = document.createElement("img");
-	red.src = "red.svg";
-	red.alt ="red";
-	
-	var blue = document.createElement("img");
-	blue.src = "blue.svg";
-	blue.alt ="blue";
-	
-	
-	
+
+	var vihr = etsiVihrea();
 	var oliVihrea;
 	//nollaaVihrea(red,blue,oliVihrea, kuvat);
 	
-	for(var i=0;i<kuvat.length;i++){
-		if(kuvat[i].alt == "redgreen" ){
-			vihr = kuvat[i];
-			oliVihrea = red;
-			vihr.parentNode.replaceChild(oliVihrea,vihr);
+	if(vihr){
+		oliVihrea=mikaVihreaOli(vihr);
+	}
+
+	if(document.getElementById("vuoronappi")){		//eli tämä tulee jos on hypätty
+		var iVert = vihr.parentNode.parentNode.rowIndex;
+		var jVert = vihr.parentNode.cellIndex;
+		var iDelta = (iIndex-iVert)/2;
+		var jDelta = (jIndex-jVert)/2;
+		if((iDelta==-1 || iDelta==0 || iDelta==1)&&(jDelta==-1 || jDelta==0 || jDelta==1)){
+			var rivit=valittu.parentNode.parentNode.getElementsByTagName("tr");
+		    var theSolu = rivit[iVert+iDelta].getElementsByTagName("td")[jVert+jDelta];
+			if(theSolu.firstChild){
+				if(onkoMaaliAlueella(vihr,valittu,"redMaali")) 
+					return;
+				valittu.appendChild(vihr);
+				
+				tarkistaPun();
+				
+				var vuoroAlue=document.getElementById("vuoroAlue");
+				var nappi = document.createElement("input","button");
+				
+				if(document.getElementById("vuoronappi")){
+					vuoroAlue.replaceChild(nappi, vuoroAlue.lastChild);
+				}
+				else	
+					vuoroAlue.appendChild(nappi);
+				
+				nappi.setAttribute("id","vuoronappi");
+				nappi.setAttribute("value","Lopeta vuoro")
+				nappi.addEventListener("click",lopetaVuoroPun);
+				
+			}
 		}
-		if(kuvat[i].alt == "bluegreen"){
-			var vihr = kuvat[i];
-			oliVihrea = blue;
-			vihr.parentNode.replaceChild(oliVihrea,vihr);
-		}
+		return;
 	}
 	
-	if(valittu.firstChild){
+	if(valittu.firstChild){ //jos klikatussa ruudussa on kuva
 		
 	var vaihdettava = valittu.firstChild;
-	
+	var redgreen = document.createElement("img");
+    redgreen.src = "green.svg";
+	redgreen.alt ="redgreen";
 	if(vaihdettava.alt == "red"){
-		var redgreen = document.createElement("img");
-        redgreen.src = "green.svg";
-	    redgreen.alt ="redgreen";
+		if(vihr){
+		
+			vihr.parentNode.replaceChild(oliVihrea,vihr);
+		}
+	
 		valittu.replaceChild(redgreen,vaihdettava);
 	}
 	
 	}
-	else{
-		if(oliVihrea){
+	else{ //jos klikattu ruutu on tyhjä
+		if(vihr){
+			if(onkoMaaliAlueella(vihr,valittu,"redMaali")) 
+					return;
 			
-			var iVert = oliVihrea.parentNode.parentNode.rowIndex;
-			var jVert = oliVihrea.parentNode.cellIndex;
+			var iVert = vihr.parentNode.parentNode.rowIndex;
+			var jVert = vihr.parentNode.cellIndex;
 		
 			if(((iVert-1)<= iIndex && iIndex<=(iVert+1))&&((jVert-1)<= jIndex && jIndex<=(jVert+1))){
-				valittu.appendChild(oliVihrea);
+				valittu.appendChild(vihr);
+				vihr.parentNode.replaceChild(oliVihrea,vihr);
+				
 				var vuoroKuva = document.getElementById("vuoro");
 				var blueVuoro = document.createElement("img");
 				blueVuoro.src = "blue.svg";
@@ -353,9 +354,9 @@ function punaisenVuoro(valittu){
 					var rivit=valittu.parentNode.parentNode.getElementsByTagName("tr");
 				    var theSolu = rivit[iVert+iDelta].getElementsByTagName("td")[jVert+jDelta];
 					if(theSolu.firstChild){
-						valittu.appendChild(oliVihrea);
+						valittu.appendChild(vihr);
 						
-						//korvataan valinnaisella vuoronvaihdolla
+						
 						
 						tarkistaPun();
 						
@@ -382,6 +383,9 @@ function punaisenVuoro(valittu){
 	}
 }
 
+/*
+Jos hyppäyksen jälkeen halutaan lopettaa vuoro tullaan tähän
+*/
 function lopetaVuoroPun(){
 	var vuoroAlue=document.getElementById("vuoroAlue");
 	vuoroAlue.removeChild(vuoroAlue.lastChild);
@@ -393,63 +397,97 @@ function lopetaVuoroPun(){
 	
 	vuoroAlue.replaceChild(blueVuoro,vuoroKuva);
 	blueVuoro.setAttribute("id","vuoro");
+	
+	var vihr = etsiVihrea();
+	var oliVihrea;
+	//nollaaVihrea(red,blue,oliVihrea, kuvat);
+	
+	oliVihrea=mikaVihreaOli(vihr);
+	vihr.parentNode.replaceChild(oliVihrea,vihr);
 }
 
 	
 
-
+/*
+Tähän tullaan kun klikataan ja on sinisen vuoro.
+TODO: tämän voisi yhdistää punaisen funktion kanssa
+*/
 function sinisenVuoro(valittu){
 	var kuvat = document.getElementsByTagName("img");
 	
 	var iIndex = valittu.parentNode.rowIndex;
 	var jIndex = valittu.cellIndex;
 	
-	var red = document.createElement("img");
-	red.src = "red.svg";
-	red.alt ="red";
-	
-	var blue = document.createElement("img");
-	blue.src = "blue.svg";
-	blue.alt ="blue";
-	
-	//JOtain pielessä
-	
+	var vihr;
 	var oliVihrea;
-	//nollaaVihrea(red,blue,oliVihrea, kuvat);
 	
-	for(var i=0;i<kuvat.length;i++){
-		if(kuvat[i].alt == "redgreen" ){
-			vihr = kuvat[i];
-			oliVihrea = red;
-			vihr.parentNode.replaceChild(oliVihrea,vihr);
+	var vihr = etsiVihrea();
+	var oliVihrea;
+
+	if(vihr){
+		oliVihrea=mikaVihreaOli(vihr);
+		
+	}
+
+	if(document.getElementById("vuoronappi")){		//eli tämä tulee jos on hypätty
+		var iVert = vihr.parentNode.parentNode.rowIndex;
+		var jVert = vihr.parentNode.cellIndex;
+		var iDelta = (iIndex-iVert)/2;
+		var jDelta = (jIndex-jVert)/2;
+		if((iDelta==-1 || iDelta==0 || iDelta==1)&&(jDelta==-1 || jDelta==0 || jDelta==1)){
+			var rivit=valittu.parentNode.parentNode.getElementsByTagName("tr");
+		    var theSolu = rivit[iVert+iDelta].getElementsByTagName("td")[jVert+jDelta];
+			if(theSolu.firstChild){
+				if(onkoMaaliAlueella(vihr,valittu,"blueMaali"))
+					return;
+				valittu.appendChild(vihr);
+				
+				tarkistaSin(); 
+				
+				var vuoroAlue=document.getElementById("vuoroAlue");
+				var nappi = document.createElement("input","button");
+				
+				if(document.getElementById("vuoronappi")){
+					vuoroAlue.replaceChild(nappi, vuoroAlue.lastChild);
+				}
+				else	
+					vuoroAlue.appendChild(nappi);
+				
+				nappi.setAttribute("id","vuoronappi");
+				nappi.setAttribute("value","Lopeta vuoro")
+				nappi.addEventListener("click",lopetaVuoroSin);
+				
+			}
 		}
-		if(kuvat[i].alt == "bluegreen"){
-			var vihr = kuvat[i];
-			oliVihrea = blue;
-			vihr.parentNode.replaceChild(oliVihrea,vihr);
-		}
+		return;
 	}
 	
 	if(valittu.firstChild){
 		
 	var vaihdettava = valittu.firstChild;
-	
+	var bluegreen = document.createElement("img");
+    bluegreen.src = "green.svg";
+	bluegreen.alt ="bluegreen";
 	if(vaihdettava.alt == "blue"){
-		var bluegreen = document.createElement("img");
-        bluegreen.src = "green.svg";
-	    bluegreen.alt ="bluegreen";
+		if(vihr){
+		
+			vihr.parentNode.replaceChild(oliVihrea,vihr);
+		}
 		valittu.replaceChild(bluegreen,vaihdettava);
 	}
 	
 	}
 	else{
-		if(oliVihrea){
-			
-			var iVert = oliVihrea.parentNode.parentNode.rowIndex;
-			var jVert = oliVihrea.parentNode.cellIndex;
+		if(vihr){
+			if(onkoMaaliAlueella(vihr,valittu,"blueMaali")) 
+				return;
+			var iVert = vihr.parentNode.parentNode.rowIndex;
+			var jVert = vihr.parentNode.cellIndex;
 			
 			if(((iVert-1)<= iIndex && iIndex<=(iVert+1))&&((jVert-1)<= jIndex && jIndex<=(jVert+1))){
-				valittu.appendChild(oliVihrea);
+				valittu.appendChild(vihr);
+				vihr.parentNode.replaceChild(oliVihrea,vihr);
+				
 				var vuoroKuva = document.getElementById("vuoro");
 				var redVuoro = document.createElement("img");
 				redVuoro.src = "red.svg";
@@ -470,17 +508,10 @@ function sinisenVuoro(valittu){
 					var rivit=valittu.parentNode.parentNode.getElementsByTagName("tr");
 				    var theSolu = rivit[iVert+iDelta].getElementsByTagName("td")[jVert+jDelta];
 					if(theSolu.firstChild){
-						valittu.appendChild(oliVihrea);
+						valittu.appendChild(vihr);
 						
 						tarkistaSin();
-						/*
-						var vuoroKuva = document.getElementById("vuoro");
-						var redVuoro = document.createElement("img");
-						redVuoro.src = "red.svg";
-						redVuoro.alt ="red";
-						vuoroKuva.parentNode.replaceChild(redVuoro,vuoroKuva);
-						redVuoro.setAttribute("id","vuoro");
-						*/
+						
 						var vuoroAlue=document.getElementById("vuoroAlue");
 						var nappi = document.createElement("input","button");
 						
@@ -503,6 +534,10 @@ function sinisenVuoro(valittu){
 	}
 }
 
+/*
+Tullaan tähän kun sininen haluaa lopettaa vuoron.
+TODO: yhdistä punaisen vastaavan funktion kanssa.
+*/
 function lopetaVuoroSin(){
 	var vuoroAlue=document.getElementById("vuoroAlue");
 	vuoroAlue.removeChild(vuoroAlue.lastChild);
@@ -514,8 +549,18 @@ function lopetaVuoroSin(){
 	
 	vuoroAlue.replaceChild(redVuoro,vuoroKuva);
 	redVuoro.setAttribute("id","vuoro");
+	
+	var vihr = etsiVihrea();
+	var oliVihrea;
+	//nollaaVihrea(red,blue,oliVihrea, kuvat);
+	
+	oliVihrea=mikaVihreaOli(vihr);
+	vihr.parentNode.replaceChild(oliVihrea,vihr);
 }
 
+/*
+Tarkistaa onko punainen voittanut, eli onko kaikki nappulat
+maalialueella*/
 function tarkistaPun(){
 	var elementit = document.getElementsByTagName("td");
 	var maali=[];
@@ -533,6 +578,10 @@ function tarkistaPun(){
 	voitto("Punainen");
 }
 
+/*
+Tarkistaa onko sininen voittanut, eli onko kaikki nappulat
+maalialuella.
+*/
 function tarkistaSin(){
 	var elementit = document.getElementsByTagName("td");
 	var maali=[];
@@ -549,22 +598,11 @@ function tarkistaSin(){
 	console.log("Sininen voitti");
 	voitto("Sininen");
 }
-/*
-function nollaaVihrea(red,blue,oliVihrea,kuvat){
-	for(var i=0;i<kuvat.length;i++){
-		if(kuvat[i].alt == "redgreen" ){
-			vihr = kuvat[i];
-			oliVihrea = red;
-			vihr.parentNode.replaceChild(oliVihrea,vihr);
-		}
-		if(kuvat[i].alt == "bluegreen"){
-			var vihr = kuvat[i];
-			oliVihrea = blue;
-			vihr.parentNode.replaceChild(oliVihrea,vihr);
-		}
-	}
-}*/
 
+
+/*
+Asettaa voittotekstin ja huolehtii että voiton jälkeen ei voi siirrellä nappuloita
+*/
 function voitto(teksti){
 	
 	document.getElementById("varod").textContent = teksti+" voitti!";
@@ -578,3 +616,133 @@ function voitto(teksti){
 		}
 	}
 }
+/*
+Etsii mahdollisen valitun nappulan
+*/
+function etsiVihrea(){
+	var vihr;
+	var kuvat = document.getElementsByTagName("img");
+	for(var i=0;i<kuvat.length;i++){
+		if((kuvat[i].alt == "redgreen")|| (kuvat[i].alt == "bluegreen")){
+			vihr = kuvat[i];
+		}
+		
+	}
+	return vihr;
+}
+/*
+Kertoo mikä valittu nappula oli ennen
+*/
+function mikaVihreaOli(vihr){
+	
+	var red = document.createElement("img");
+	red.src = "red.svg";
+	red.alt ="red";
+	
+	var blue = document.createElement("img");
+	blue.src = "blue.svg";
+	blue.alt ="blue";
+	
+	var oliVihrea;
+	if(vihr.alt =="redgreen"){
+			oliVihrea = red;
+		}
+	if(vihr.alt =="bluegreen"){
+			oliVihrea = blue;
+	}
+	return oliVihrea;
+}
+
+/*
+Palauttaa tosi, jos siirrettävä on maalialueella ja siirron kohde ei.
+Eli tällä voidaan estää siirto maalialueelta pois.
+*/
+function onkoMaaliAlueella(vihr,valittu,maali){
+	return ((vihr.parentNode.title == maali) && (valittu.title != maali));
+}
+
+/*
+Täällä olen yrittänyt saada skaalautuvuutta aikaan.
+Tuli ongelmia noiden svg-pallojen kanssa, en saanut niitä millään
+näkyviin taulukossa vaikka ne ilmestyivät selaimen dom-puuhun...
+*/
+
+/*
+function luoPalloSvg(syote,vari){
+	var pallo = document.createElement("svg");
+	pallo.setAttribute("xmlns","http://www.w3.org/2000/svg");
+	pallo.setAttribute("xmlns:xlink","http://www.w3.org/1999/xlink");
+	
+	var leveys = 0.9*tutkiRuudunLeveys(syote);
+	console.log(leveys);
+	pallo.setAttribute("viewBox","0 0 "+leveys+" "+leveys);
+	//pallo.setAttribute("viewbox","0 0 "leveys+" "+leveys);
+	
+	
+	pallo.setAttribute("width",leveys+"px");
+	pallo.setAttribute("height",leveys+"px");
+	
+	var ympyra= document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	/*ympyra.setAttribute("cx",leveys/2);
+	ympyra.setAttribute("cy",leveys/2);
+	ympyra.setAttribute("r",(leveys/2-1));
+	
+	ympyra.setAttributeNS(null, "cx", leveys/2);
+    ympyra.setAttributeNS(null, "cy", leveys/2);
+    ympyra.setAttributeNS(null, "r",  (leveys/2-1));
+    //ympyra.setAttributeNS(null, "fill", "green"); 
+	
+	if(vari=="red"){
+		ympyra.setAttributeNS(null,"style","stroke:#006600; fill:#ff0000");
+	}
+	if(vari=="blue"){
+		ympyra.setAttributeNS(null,"style","stroke:#006600; fill:#0000ff");
+	}
+	if(vari=="green"){
+		ympyra.setAttributeNS(null,"style","stroke:#006600; fill:#00ff00");
+	}
+	pallo.appendChild(ympyra);
+	return pallo;
+}
+*/
+/*
+function luoPallo(syote,vari){
+	var leveys=tutkiRuudunLeveys(syote)*0.9;
+	if(vari=="red"){
+		var red = document.createElement("img");
+		red.src = "red.svg";
+		red.alt ="red";
+		red.setAttribute("style","width:"+leveys+"; height:"+leveys);
+		return red;
+	}
+	if(vari=="blue"){
+		var blue = document.createElement("img");
+		blue.src = "blue.svg";
+		blue.alt ="red";
+		blue.setAttribute("style","width:"+leveys+"; height:"+leveys);
+		return blue;
+	}
+	
+}*/
+/*
+function tutkiRuudunLeveys(syote){
+	var html = document.getElementsByTagName("html")[0];
+	var h =document.getElementsByTagName("h1")[0];
+	var p =document.getElementsByTagName("p")[0];
+	var f =document.getElementsByTagName("form")[0];
+	var ruutuLeveys;
+	
+	var nayttoLeveys = html.scrollWidth;
+	var nayttoKorkeus = html.scrollHeight;
+	
+	var korkeusMuut = h.scrollHeight+p.scrollHeight+f.scrollHeight;
+	var korkeusRuud = nayttoKorkeus - korkeusMuut;
+	
+	if(korkeusRuud < nayttoLeveys){
+		ruutuLeveys =korkeusRuud/syote;
+	}
+	else{
+		ruutuLeveys =nayttoLeveys/syote;
+	}
+	return ruutuLeveys;
+}*/
